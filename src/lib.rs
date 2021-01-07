c12e10929bfed43b66d5f4e4869cdc02c3db8347
zoe <zoe211@163.com>
2021-01-07 23:55:01 +0800

    timeout

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,9 @@
-pub mod stream_vec;
+#[cfg(feature = "stream_vec")]
+mod stream_vec;
+#[cfg(feature = "timeout")]
+mod timeout;
+
+#[cfg(feature = "stream_vec")]
+pub use stream_vec::StreamVec;
+#[cfg(feature = "timeout")]
+pub use timeout::Timeout;
