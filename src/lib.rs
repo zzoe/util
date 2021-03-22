4585a48fe4b6dfd9a6db49a10f8264638d82c724
zoe <zoe211@163.com>
2021-03-22 12:57:48 +0800

    pub use {}

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,15 @@
 #[cfg(feature = "select")]
-mod select;
+pub use select::Select;
+#[cfg(feature = "timeout")]
+pub use timeout::Timeout;
+#[cfg(feature = "esb")]
+pub use {esb::esb_json_to_xml, esb::esb_xml_to_json};
 
 #[cfg(feature = "select")]
-pub use select::Select;
+mod select;
 
 #[cfg(feature = "timeout")]
 mod timeout;
 
-#[cfg(feature = "timeout")]
-pub use timeout::Timeout;
-
 #[cfg(feature = "esb")]
 mod esb;
-
-#[cfg(feature = "esb")]
-pub use esb::esb_json_to_xml;
-pub use esb::esb_xml_to_json;
