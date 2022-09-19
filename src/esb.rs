94469cf44f52290fc2309214315b4e7e45118a2a
zoe <zoe211@163.com>
2022-09-19 14:15:52 +0800

    quick-xml 版本更新

--- src/esb.rs
+++ src/esb.rs
@@ -1,6 +1,7 @@
 use std::io::Write;
 use std::option::Option::Some;
 
+use bytes::buf::Writer as BufWriter;
 use bytes::{Buf, BufMut, Bytes, BytesMut};
 use quick_xml::events::{BytesEnd, BytesStart, BytesText, Event};
 use quick_xml::{Error, Reader, Result, Writer};
@@ -21,38 +22,44 @@ pub fn esb_xml_to_json(b: Bytes) -> Result<Bytes> {
     let mut field_need_quote = false;
 
     loop {
-        match reader.read_event(&mut buf)? {
-            Event::Start(s) => match &*s.name().to_ascii_lowercase() {
-                b"service" | b"struct" => msg.put_u8(b'{'),
-                b"body" => msg.put_slice(br#""BODY":{"#),
-                b"array" => msg.put_u8(b'['),
-                b"data" => {
-                    let mut attrs = s.attributes();
-                    while let Some(Ok(a)) = attrs.next() {
-                        if a.key.to_ascii_lowercase().eq(b"name") {
-                            msg.put_u8(b'"');
-                            msg.put_slice(&*a.value);
-                            msg.put_slice(br#"":"#);
-                            break;
+        match reader.read_event_into(&mut buf)? {
+            Event::Start(s) => {
+                match std::str::from_utf8(s.name().as_ref())
+                    .map(|a| a.to_ascii_lowercase())
+                    .unwrap_or_default()
+                    .as_ref()
+                {
+                    "service" | "struct" => msg.put_u8(b'{'),
+                    "body" => msg.put_slice(br#""BODY":{"#),
+                    "array" => msg.put_u8(b'['),
+                    "data" => {
+                        let mut attrs = s.attributes();
+                        while let Some(Ok(a)) = attrs.next() {
+                            if a.key.into_inner().to_ascii_lowercase().eq(b"name") {
+                                msg.put_u8(b'"');
+                                msg.put_slice(&*a.value);
+                                msg.put_slice(br#"":"#);
+                                break;
+                            }
                         }
                     }
-                }
-                b"field" => {
-                    is_field = true;
-                    let mut attrs = s.attributes();
-                    while let Some(Ok(a)) = attrs.next() {
-                        if a.key.to_ascii_lowercase().eq(b"type")
-                            && (a.value.to_ascii_lowercase().eq(b"byte")
-                                || a.value.to_ascii_lowercase().eq(b"string"))
-                        {
-                            field_need_quote = true;
-                            break;
+                    "field" => {
+                        is_field = true;
+                        let mut attrs = s.attributes();
+                        while let Some(Ok(a)) = attrs.next() {
+                            if a.key.into_inner().to_ascii_lowercase().eq(b"type")
+                                && (a.value.to_ascii_lowercase().eq(b"byte")
+                                    || a.value.to_ascii_lowercase().eq(b"string"))
+                            {
+                                field_need_quote = true;
+                                break;
+                            }
                         }
                     }
+                    _ => {}
                 }
-                _ => {}
-            },
-            Event::End(e) => match &*e.name().to_ascii_lowercase() {
+            }
+            Event::End(e) => match &*e.name().into_inner().to_ascii_lowercase() {
                 b"service" => {
                     truncate(&mut msg, b",");
                     msg.put_u8(b'}');
@@ -67,7 +74,7 @@ pub fn esb_xml_to_json(b: Bytes) -> Result<Bytes> {
                 }
                 _ => {}
             },
-            Event::Empty(s) => match &*s.name().to_ascii_lowercase() {
+            Event::Empty(s) => match &*s.name().into_inner().to_ascii_lowercase() {
                 b"struct" => msg.put_slice(b"{}"),
                 b"array" => msg.put_slice(b"[]"),
                 b"field" => msg.put_slice(b"null,"),
@@ -112,25 +119,21 @@ pub fn esb_json_to_xml(b: Bytes) -> Result<Bytes> {
         serde_json::from_reader(&*b).map_err(|e| Error::UnexpectedToken(e.to_string()))?;
     write_value(&mut writer, &json, Level::Service)?;
 
-    let elem = BytesEnd::borrowed("service".as_bytes());
+    let elem = BytesEnd::new("service");
     writer.write_event(Event::End(elem))?;
 
     Ok(writer.into_inner().into_inner().into())
 }
 
-fn write_value(
-    writer: &mut Writer<bytes::buf::Writer<Vec<u8>>>,
-    value: &Value,
-    level: Level,
-) -> Result<()> {
+fn write_value(writer: &mut Writer<BufWriter<Vec<u8>>>, value: &Value, level: Level) -> Result<()> {
     match value {
         Value::String(s) => {
-            let fields = s.split(",").collect::<Vec<&str>>();
+            let fields = s.split(',').collect::<Vec<&str>>();
             if fields.len() < 3 {
                 return Ok(());
             }
 
-            let mut elem = BytesStart::borrowed_name("field".as_bytes());
+            let mut elem = BytesStart::new("field");
             elem.push_attribute(("type", match_field(fields[0])));
             elem.push_attribute(("length", fields[1]));
             elem.push_attribute(("scale", fields[2]));
@@ -139,14 +142,14 @@ fn write_value(
             if fields.len() > 3 {
                 //当值里面包含","时会被分隔，这里重新合并。
                 let field: String = fields[3..].join(",");
-                let text = BytesText::from_plain_str(&*field);
+                let text = BytesText::new(&*field);
                 writer.write_event(Event::Text(text))?;
             }
 
             writer.write_event(Event::End(elem.to_end()))?;
         }
         Value::Array(a) => {
-            let elem = BytesStart::borrowed_name("array".as_bytes());
+            let elem = BytesStart::new("array");
             writer.write_event(Event::Start(elem.clone()))?;
             for v in a {
                 write_value(writer, v, Level::Field)?;
@@ -163,19 +166,19 @@ fn write_value(
 }
 
 fn write_map(
-    writer: &mut Writer<bytes::buf::Writer<Vec<u8>>>,
+    writer: &mut Writer<BufWriter<Vec<u8>>>,
     object: &Map<String, Value>,
     level: Level,
 ) -> Result<()> {
     if level == Level::Field {
-        let elem = BytesStart::borrowed_name("struct".as_bytes());
+        let elem = BytesStart::new("struct");
         writer.write_event(Event::Start(elem))?;
     }
 
     for (k, v) in object.iter() {
         match k.as_str() {
             name if level == Level::Service && name.to_ascii_lowercase().eq("body") => {
-                let elem = BytesStart::borrowed_name("body".as_bytes());
+                let elem = BytesStart::new("body");
                 writer.write_event(Event::Start(elem.clone()))?;
                 if let Value::Object(o) = v {
                     write_map(writer, o, Level::Body)?;
@@ -183,7 +186,7 @@ fn write_map(
                 writer.write_event(Event::End(elem.to_end()))?;
             }
             name if level == Level::Service => {
-                let elem = BytesStart::borrowed_name(match_head(name).as_bytes());
+                let elem = BytesStart::new(match_head(name));
                 writer.write_event(Event::Start(elem.clone()))?;
                 write_data_start(writer, name)?;
                 write_value(writer, v, Level::Field)?;
@@ -199,21 +202,21 @@ fn write_map(
     }
 
     if level == Level::Field {
-        let elem = BytesEnd::borrowed("struct".as_bytes());
+        let elem = BytesEnd::new("struct");
         writer.write_event(Event::End(elem))?;
     }
 
     Ok(())
 }
 
-fn write_data_start(writer: &mut Writer<bytes::buf::Writer<Vec<u8>>>, name: &str) -> Result<()> {
-    let mut data = BytesStart::borrowed_name("data".as_bytes());
+fn write_data_start(writer: &mut Writer<BufWriter<Vec<u8>>>, name: &str) -> Result<()> {
+    let mut data = BytesStart::new("data");
     data.push_attribute(("name", name));
     writer.write_event(Event::Start(data))
 }
 
-fn write_data_end(writer: &mut Writer<bytes::buf::Writer<Vec<u8>>>) -> Result<()> {
-    let data = BytesEnd::borrowed("data".as_bytes());
+fn write_data_end(writer: &mut Writer<BufWriter<Vec<u8>>>) -> Result<()> {
+    let data = BytesEnd::new("data");
     writer.write_event(Event::End(data))
 }
 
